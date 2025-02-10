f5e9169e8b0b3245f6b1cb9dc817bec08bd9d759
Fujiirkfc <fujiiken026@gmail.com>
2025-02-11 01:45:05 +0900

    initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.jonas.io/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = 'ad995527-778d-455b-8add-6bfbd3a1f87f';
+export const MODAL_CLOSE_SEC = 2.5;
